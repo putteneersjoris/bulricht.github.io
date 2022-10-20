6d303aa6edb86009c78814c82fd0b34033a561ee
ulrichtsblr <39812764+ulrichtsblr@users.noreply.github.com>
2022-10-20 23:55:35 +0200

    add action

--- data.js
+++
@@ -1,9 +0,0 @@
-var imageUrls = [
-	"./data/Aphex-Gallery-10-1.jpg",
-	"./data/aphex-twin-fold_custom-fb3ac721e4ede6563bf584dda2ad0432f3c09f45.jpg",
-	"./data/aphex-twin.jpg",
-	"./data/Richard_d_james_album_cover.jpg",
-]
-var textUrls = [
-	"./data/test.txt",
-]
