333807e98e3b61d5391c4b1fc5d0e5cb985c0156
ulrichtsblr <39812764+ulrichtsblr@users.noreply.github.com>
2022-10-20 23:45:14 +0200

    add data.py

---
+++ data.py
@@ -0,0 +1,14 @@
+import os
+path = "./data"
+with open("data.js", "w", encoding="utf8") as f:
+    items = os.listdir(path)
+    f.write("var imageUrls = [\n")
+    for item in items:
+        if item.endswith((".png", ".jpg", ".jpeg")):
+            f.write(f'\t"{path}/{item}",\n')
+    f.write("]\n")
+    f.write("var textUrls = [\n")
+    for item in items:
+        if item.endswith(".txt"):
+            f.write(f'\t"{path}/{item}",\n')
+    f.write("]\n")
