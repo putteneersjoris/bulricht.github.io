2b1c7406279a48782100fd3065fb40851ae19c09
ulrichtsblr <39812764+ulrichtsblr@users.noreply.github.com>
2022-10-20 23:23:46 +0200

    add data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+var imageUrls = [
+    "./data/Aphex-Gallery-10-1.jpg",
+    "./data/aphex-twin-fold_custom-fb3ac721e4ede6563bf584dda2ad0432f3c09f45.jpg",
+    "./data/aphex-twin.jpg",
+    "./data/Richard_d_james_album_cover.jpg",
+];
