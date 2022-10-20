a4cea6bf7544f5aeabd4e9e80cbd4bcb10486712
ulrichtsblr <39812764+ulrichtsblr@users.noreply.github.com>
2022-10-21 00:42:39 +0200

    add pkg

--- pkg/data.py
+++ pkg/data.py
@@ -1,15 +1,14 @@
 import os
-parent = "./src"
-path = f"{parent}/data"
-with open(f"{parent}/data.js", "w", encoding="utf8") as f:
-    items = os.listdir(path)
+path = "data"
+with open("./src/data.js", "w", encoding="utf8") as f:
+    items = os.listdir(f"./src/{path}")
     f.write("var imageUrls = [\n")
     for item in items:
         if item.endswith((".png", ".jpg", ".jpeg")):
-            f.write(f'\t"{path}/{item}",\n')
+            f.write(f'\t"./{path}/{item}",\n')
     f.write("]\n")
     f.write("var textUrls = [\n")
     for item in items:
         if item.endswith(".txt"):
-            f.write(f'\t"{path}/{item}",\n')
+            f.write(f'\t"./{path}/{item}",\n')
     f.write("]\n")
